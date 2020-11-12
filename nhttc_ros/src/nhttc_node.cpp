2bec8f7fc831065a81e39847561a6837b80c5cb7
SidharthTalia <sidharthtalia@ymail.com>
2020-11-12 18:05:59 +0530

    Update nhttc_node.cpp

--- nhttc_ros/src/nhttc_node.cpp
+++ nhttc_ros/src/nhttc_node.cpp
@@ -3,7 +3,6 @@
 #include "geometry_msgs/Point.h"
 #include "geometry_msgs/PoseStamped.h"
 #include "ackermann_msgs/AckermannDriveStamped.h"
-#include "tf2/LinearMath/Quaternion.h" //keeping these because they may be useful in the future?
 #include "tf2_geometry_msgs/tf2_geometry_msgs.h"
 #include <string>
 #include <sstream>
@@ -13,7 +12,7 @@ class nhttc_ros
 {
 public:
   ros::Subscriber sub_pose,sub_twist;
-  ros::Subscriber sub_other_pose[4],sub_other_odom[4];
+  ros::Subscriber sub_other_pose[8],sub_other_odom[8];
   ros::Subscriber sub_goal;
   ros::Publisher pub_cmd;
 
@@ -22,63 +21,47 @@ public:
   char index;
   std::ostringstream s;
 
-  float cur_state[7];
-  float other_state[4][7];
-  float goal_pose[3];
+  Eigen::Vector2f goal;
   int own_index;
+  int solver_time;
   float max_velocity;
   SGDOptParams global_params;
   std::vector<Agent> agents; //all agents
+  int count;
 
-  // Agent agent; // ego agent
   std::vector<TTCObstacle*> obstacles = BuildObstacleList(agents);
 
-  float get_velocity_bf(float vx,float vy,float theta) //get body frame velocity.
+  void agent_setup(int i)
   {
-    return vx*cosf(theta) + vy*sinf(theta); //ROS uses the ENU reference frame, so this theta is wrt to the x axis. 
-  }
-
-  void PoseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg)
-  {
-    float rpy[3];
-    rpy_from_quat(rpy,msg);
-    cur_state[0] = msg->pose.position.x;
-    cur_state[1] = msg->pose.position.y;
-    cur_state[2] = rpy[2]; 
-  }
-  void OdomCallback(const nav_msgs::Odometry::ConstPtr& msg)
-  {
-    cur_state[3] = msg->twist.twist.linear.x;
-    cur_state[4] = msg->twist.twist.linear.y;
-    cur_state[5] = msg->twist.twist.angular.z;
-    cur_state[6] = get_velocity_bf(cur_state[3],cur_state[4],cur_state[2]);
+    Eigen::Vector2f goal(0.0,0.0);
+    Eigen::VectorXf pos = Eigen::VectorXf::Zero(3);
+    agents.emplace_back(GetAgentParts(6, pos, true, goal), global_params);
   }
 
   void OtherPoseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg, int i)
   {
+    if(count<i)
+    {
+      int last_count = count+1;
+      count = i;
+      for(int i=last_count; i <= count;i++)
+      {
+        agent_setup(i);
+      }
+    }
     float rpy[3];
+    Eigen::VectorXf x_o = Eigen::VectorXf::Zero(3);
     rpy_from_quat(rpy,msg);
-    other_state[i][0] = msg->pose.position.x;
-    other_state[i][1] = msg->pose.position.y;
-    other_state[i][2] = rpy[2]; 
-  }
-
-  void OtherOdomCallback(const nav_msgs::Odometry::ConstPtr& msg, int i)
-  {
-    other_state[i][3] = msg->twist.twist.linear.x;
-    other_state[i][4] = msg->twist.twist.linear.y;
-    other_state[i][5] = msg->twist.twist.angular.z;
-    other_state[i][6] = get_velocity_bf(other_state[i][3],other_state[i][4],other_state[i][2]);
-
+    x_o[0] = msg->pose.position.x;
+    x_o[1] = msg->pose.position.y;
+    x_o[2] = rpy[2];
+    agents[i].SetEgo(x_o);
   }
 
   void GoalCallback(const geometry_msgs::PoseStamped::ConstPtr& msg)
   {
-    float rpy[3];
-    rpy_from_quat(rpy,msg);
-    goal_pose[0] = msg->pose.position.x;
-    goal_pose[1] = msg->pose.position.y;
-    goal_pose[2] = rpy[2]; // goal yaw. if mike is short for micheal, is yaw short for yee-haw? I would very much like it to be so. TODO: remove this comment for being "unacademic" 
+    goal[0] = msg->pose.position.x;
+    goal[1] = msg->pose.position.y;
   }
 
   void send_commands(float speed, float steer) // speed is in m/s. steering is in radians
@@ -94,16 +77,15 @@ public:
   {
 
     nh.getParam("car_name",self_name);
-    sub_pose = nh.subscribe("/" + self_name +"/car_pose",10,&nhttc_ros::PoseCallback,this);
-    sub_twist = nh.subscribe("/"+ self_name +"/vesc/odom",10,&nhttc_ros::OdomCallback,this);
-    Eigen::Vector2f goal(0.0,0.0);
-    Eigen::VectorXf pos = Eigen::VectorXf::Zero(3);
+    if(not nh.getParam("solver_time",solver_time))
+    {
+      solver_time = 10; // 10 ms solver time for each agent.
+    }
     max_velocity = 0.3;
     ConstructGlobalParams(&global_params);
-
+    count = -1; //number of agents. start from -1.
     // solution for multi-agent setting
-    // insert hackerman_meme.jpg here
-    for(int i=0;i<4;i++)
+    for(int i=0;i<8;i++) //limit for i can be more but not less than the total no. of cars.
     { //you'd think that with ROS being such a widely used backend that it would be simple to convert an integer to a string but nooooooo I have to make it a stringstream, get the .str() of it, then get the .c_str() of that to make it work
       s.str("");
       s<<"/car";
@@ -114,22 +96,6 @@ public:
       }
       s<<"/car_pose";
       sub_other_pose[i] = nh.subscribe<geometry_msgs::PoseStamped>((s.str()).c_str(),10,boost::bind(&nhttc_ros::OtherPoseCallback,this,_1,i));
-      s.str("");
-      s<<"/car";
-      s<<i+1;
-      s<<"/vesc/odom";
-      sub_other_odom[i] = nh.subscribe<nav_msgs::Odometry>((s.str()).c_str(),10,boost::bind(&nhttc_ros::OtherOdomCallback,this,_1,i));
-
-      ROS_INFO("nhttc_constructor");
-      agents.emplace_back(GetAgentParts(6, pos, true, goal), global_params);
-      // Agent agent((GetAgentParts(6, pos, true, goal), global_params));
-      // agents.push_back(agent);
-    }
-    for(int i=0;i<4;i++)
-    {
-      agents[i].prob->params.radius = 0.2;
-      agents[i].prob->params.safety_radius = 0.05;
-      agents[i].prob->params.max_ttc = 20;//std::min(6/max_velocity,20);
     }
     sub_goal = nh.subscribe("/"+self_name+"/move_base_simple/goal",10,&nhttc_ros::GoalCallback,this);
     pub_cmd = nh.advertise<ackermann_msgs::AckermannDriveStamped>("/"+ self_name +"/mux/ackermann_cmd_mux/input/navigation",10);
@@ -150,89 +116,13 @@ public:
 
   void plan()
   {
-    /*
-    ...
-    code for planning to be inserted here
-    
-    state:
-    state[0] = x;
-    state[1] = y;
-    state[2] = yaw/heading angle; (theta)
-    state[3] = velocity in x direction
-    state[4] = velocity in y direction
-    state[5] = rate of rotation about z axis.
-    state[6] = body frame speed (forward is +ve, backward is -ve)
-    ...
-    */
-
-    /* Preferred interface: 
-    nhttc_agent.set_params(param); // params include time allowed for planning, time-step, etc. More on this later as I build it out.
-
-    nhttc_agent.set_obstacles(list_of_obstacle_states); // void nhttc_class::set_obstacles(float agent_states[4][6])
-      This list could be built with a global list enhanced with additional obstacles as necessary. Most of our programming will likely go into
-      generating and modifying the obstacles going into the planner.
-      Individual Obstacle requirements: 
-        1. Type (Velocity, MuSHRCar, PixelBlock, etc.) 
-        2. type-specific State 
-        3. type-specific controls
-
-    nhttc_agent.set_goals(goals); 
-    The goals are either for the agent AND all the obstacles, or just for the agents. I think its just for the current agent but I'll have
-    to double check. This is where the carrot goal will come in, i.e. the input from the global planner.
-
-
-    nhttc_agent.set_ego(cur_state); // void nhttc_class:set_ego(float self_state[6])
-      Agent State Requirements:
-        1. Type (can just be initialized on creation so you don't constantly pass it in)
-        2. type-specific State (X, Y, Heading Angle) (I have to double check if its with respect to the global or car frame)
-        3. type-specific controls (Speed, Steering angle)
-
-    nhttc_agent.get_controls(return_parameter);
-      This will direcly return the new control actions very easily
-
-    */
-    // TODO: figure out how to init an agent.
-    // set poses of all agents (including ego agent)
-    Eigen::VectorXf x_o = Eigen::VectorXf::Zero(3);
-    for(int i = 0;i< 4;i++)
-    {
-      x_o[0] = other_state[i][0];
-      x_o[1] = other_state[i][1];
-      x_o[2] = other_state[i][2];
-      agents[i].SetEgo(x_o);
-    }
     // create obstacle list.
     obstacles = BuildObstacleList(agents);
-
-    Eigen::Vector2f goal;
-
-    if(own_index==0)
-    {
-      goal[0] = 0;
-      goal[1] = 0;
-    }
-    if(own_index==1)
-    {
-      goal[0] = 10;
-      goal[1] = 0;
-    }
-    if(own_index==2)
-    {
-      goal[0] = 0;
-      goal[1] = 10;
-    }
-    if(own_index==3)
-    {
-      goal[0] = 10;
-      goal[1] = 10;
-    }
-
-    agents[own_index].SetPlanTime(10); //200 ms planning window
+    agents[own_index].SetPlanTime(solver_time); //200 ms planning window
     agents[own_index].SetObstacles(obstacles, size_t(own_index));
     agents[own_index].UpdateGoal(goal);
     Eigen::VectorXf controls = agents[own_index].UpdateControls();
 
-    // ROS_INFO("%f",agents[own_index].prob->params.max_ttc);
     float speed = controls[0]; //speed in m/s
     float steering_angle = controls[1]; //steering angle in radians. +ve is left. -ve is right 
     send_commands(speed,steering_angle); //just sending out anything for now;
@@ -243,12 +133,13 @@ public:
 
 int main(int argc, char** argv)
 {
-  ros::init(argc,argv,"location_monitor");
+  ros::init(argc,argv,"nhttc_local_planner");
   ros::NodeHandle nh("~");
   nhttc_ros local_planner(nh);
   ros::Rate r(50);
-  for(int i = 0; i < 100;i++)
+  for(int i = 0; i < 250;i++) //wait for 5 seconds: this can be removed for the real world; it corresponds to the time taken by the rest of the stuff to init
   {
+    ros::spinOnce(); // spin but don't plan anything.
     r.sleep();
   }
   while(ros::ok)
