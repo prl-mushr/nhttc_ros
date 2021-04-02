f2c3fc21df1238fc95a3022106ec46bb0ccd07bc
naughtyStark <sidharthtalia@ymail.com>
2021-04-02 23:46:48 +0530

    doc-strings. doc strings everywhere

--- nhttc_ros/src/nhttc_node.cpp
+++ nhttc_ros/src/nhttc_node.cpp
@@ -10,6 +10,12 @@
 #include <sstream>
 #include "nhttc_interface.h"
 #include <boost/algorithm/string.hpp>
+
+/**
+ * nhttc_ros class.
+ *
+ * This class provides a ros interface to the nhttc backend
+ */
 class nhttc_ros
 {
 public:
@@ -49,7 +55,13 @@ public:
   ros::master::V_TopicInfo master_topics;
 
   ros::Time begin;
-
+  /**
+   * setting up agents
+   *
+   * This function initializes the nhttc agents with a preset starting pose and goal
+   *
+   * @param takes the index value corresponding to that agent
+   */
   void agent_setup(int i)
   {
     Eigen::Vector2f goal(0.0,0.0);
@@ -57,7 +69,32 @@ public:
     agents.emplace_back(GetAgentParts(6, pos, true, goal), global_params);
   }
 
-  void OtherPoseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg, int i)
+  /**
+   * Quaternion to euler angle converter
+   *
+   * Converts quaternion representation to euler angle representation. rpy = [roll, pitch, yaw]. rpy is in radians, following the ENU reference frame
+   * @params euler angle array (float), pose message (geometry_msgs::PoseStamped)
+   */
+  void rpy_from_quat(float rpy[3],const geometry_msgs::PoseStamped::ConstPtr& msg) 
+  { 
+    float q[4];
+    q[0] = msg->pose.orientation.x;
+    q[1] = msg->pose.orientation.y;
+    q[2] = msg->pose.orientation.z;
+    q[3] = msg->pose.orientation.w;
+    rpy[2] = atan2f(2.0f * (q[0] * q[1] + q[2] * q[3]),1 - 2 * (q[1] * q[1] + q[2] * q[2]));
+    rpy[0] = asinf(2.0f * (q[0] * q[2] - q[3] * q[1]));
+    rpy[1] = atan2f(2.0f * (q[0] * q[3] + q[1] * q[2]), 1.0f - 2.0f * (q[2] * q[2] + q[3] * q[3]));
+  }
+
+  /**
+   * Pose callback
+   *
+   * This function listens to the pose-publishers of all the agents
+   *
+   * @param takes the pose msg (geometry_msgs::PoseStamped) and the index value corresponding to the agent.
+   */
+  void PoseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg, int i)
   {
     float rpy[3];
     Eigen::VectorXf x_o = Eigen::VectorXf::Zero(3);
@@ -68,7 +105,14 @@ public:
     agents[i].SetEgo(x_o);
   }
 
-  void OtherControlCallback(const ackermann_msgs::AckermannDriveStamped::ConstPtr& msg, int i)
+  /**
+   * Control callback
+   *
+   * This function listens to the control-publishers of all the agents. This is required by the nhttc as it takes into account the current state and actions of the other agents
+   *
+   * @param takes the control msg (ackermann_msgs::AckermannDriveStamped) and the index value corresponding to the agent.
+   */
+  void ControlCallback(const ackermann_msgs::AckermannDriveStamped::ConstPtr& msg, int i)
   {
     Eigen::VectorXf controls = Eigen::VectorXf::Zero(2);
     controls[0] = msg->drive.speed;
@@ -79,10 +123,17 @@ public:
     }
   }
 
+  /**
+   * Waypoint callback
+   *
+   * This function listens to the waypoint messages (geometry_msgs::PoseArray) published by the global planner.
+   *
+   * @param takes the waypoint message.
+   */
   void WPCallBack(const geometry_msgs::PoseArray::ConstPtr& msg)
   {
     Eigen::Vector2f wp,wp_last;
-    int num = msg->poses.size();//sizeof(msg->poses)/sizeof(msg->poses[0]);
+    int num = msg->poses.size();
     waypoints.clear(); // reset
     float time_stamp=0;
     time_stamps.clear();
@@ -106,7 +157,14 @@ public:
     time_index = 1; //reset
     begin = ros::Time::now();// + ros::Duration(0.02); // add 0.02 seconds corresponding to the 50 hz update rate.
   }
-   
+
+  /**
+   * Single-goal callback
+   *
+   * Used for a single-goal point mission.
+   *
+   * @param goal point message (geometry_msgs::PoseStamped)
+   */
   void GoalCallback(const geometry_msgs::PoseStamped::ConstPtr& msg)
   {
     goal[0] = msg->pose.position.x;
@@ -115,7 +173,14 @@ public:
     goal_received = true;
   }
 
-  void send_commands(float speed, float steer) // speed is in m/s. steering is in radians
+  /**
+   * Send throttle/steering commands
+   *
+   * sends commands to the vesc/mux/input/navigation topic
+   *
+   * @param speed (float, m/s) steering angle (float, radius)
+   */
+  void send_commands(float speed, float steer)
   {
     ackermann_msgs::AckermannDriveStamped output_msg;
     output_msg.header.stamp = ros::Time::now();
@@ -124,7 +189,11 @@ public:
     output_msg.drive.speed = speed;
     pub_cmd.publish(output_msg);
   }
-
+  /**
+   * Publish position of current goal-point for visualization
+   *
+   * Publishes the current nhttc-goal to the /car_/cur_goal topic. Used for visualizing where the car is currently trying to go.
+   */
   void viz_publish()
   {
     geometry_msgs::PoseStamped output_msg;
@@ -135,6 +204,12 @@ public:
     viz_pub.publish(output_msg);
   }
 
+  /**
+   * Publish position of current time-point for visualization
+   *
+   * Publishes the current time-point to the /car_/time_goal topic. Used for visualizing the point with which the car is trying to
+   * synchronize it's timing.
+   */
   void time_viz_publish()
   {
     geometry_msgs::PoseStamped output_msg;
@@ -145,11 +220,17 @@ public:
     time_pub.publish(output_msg);
   }
 
+  /**
+   * nhttc ros constructor
+   *
+   * Sets up the publishers, subscribers and the whole shebang
+   */
   nhttc_ros(ros::NodeHandle &nh)
   {
-    goal_received = false; // start with the assumption that your life has no goal.
-    current_wp_index = 0;
-    max_index = 0;
+    goal_received = false; // start with the assumption that the car has no goal
+    current_wp_index = 0; // set current waypoint index to 0
+    max_index = 0; //set max_waypoint index to 0
+    // get all the params
     nh.getParam("car_name",self_name);
     if(not nh.getParam("solver_time",solver_time))
     {
@@ -174,8 +255,11 @@ public:
 
     ConstructGlobalParams(&global_params);
     count = -1; //number of agents. start from -1.
-    // solution for multi-agent setting
-    // set 8 -> make a param.
+    // sleep rate. Wait for all the agents to be set up before you start counting.
+    /*
+     * The following code-block listens to all the published topics and filters out those which end with /car_pose or /mocap_pose.
+     * When it finds the corresponding topic, it creates the subscribers/publishers for that agent and increases the count for the number of agents.
+     */
     ros::Rate r(1);
     for(int i=0;i<5;i++)
     {
@@ -190,7 +274,7 @@ public:
       if(info.datatype == "geometry_msgs/PoseStamped")
       {
         boost::split(strs, info.name, boost::is_any_of("/"));
-        if(strs[2]=="car_pose")
+        if(strs[2]=="car_pose" or strs[2]=="mocap_pose")
         {
           count++;
           if(strs[1] == self_name)
@@ -199,25 +283,33 @@ public:
           }
           agent_setup(count);
 
-          sub_other_pose[count] = nh.subscribe<geometry_msgs::PoseStamped>(info.name,10,boost::bind(&nhttc_ros::OtherPoseCallback,this,_1,count));
+          sub_other_pose[count] = nh.subscribe<geometry_msgs::PoseStamped>(info.name,10,boost::bind(&nhttc_ros::PoseCallback,this,_1,count));
           s.str("");
           s<<"/";
           s<<strs[1];
           s<<"/mux/ackermann_cmd_mux/input/navigation";
-          sub_other_control[count] = nh.subscribe<ackermann_msgs::AckermannDriveStamped>((s.str()).c_str(),10,boost::bind(&nhttc_ros::OtherControlCallback,this,_1,count));
+          sub_other_control[count] = nh.subscribe<ackermann_msgs::AckermannDriveStamped>((s.str()).c_str(),10,boost::bind(&nhttc_ros::ControlCallback,this,_1,count));
         }
       }
     }
+    // set up all the publishers/subscribers
     sub_wp = nh.subscribe("/"+self_name+"/waypoints",10,&nhttc_ros::WPCallBack,this);
     pub_cmd = nh.advertise<ackermann_msgs::AckermannDriveStamped>("/"+ self_name +"/mux/ackermann_cmd_mux/input/navigation",10);
     viz_pub = nh.advertise<geometry_msgs::PoseStamped>("/"+self_name+"/cur_goal",10);
     time_pub = nh.advertise<geometry_msgs::PoseStamped>("/"+self_name+"/time_goal",10);
-    ROS_INFO("node started");
+    ROS_INFO("node started"); // tell the world that the node has initialized.
   }
 
+  /**
+   * Parameter setting for the ego-agent.
+   *
+   * Sets up the tuning parameters for the ego-agent. These parameters are taken from the launch file. The tuning parameters include 
+   * 1) The carrot-goal ratio: (lookahead distance)/(turning radius of the car)
+   * 2) max_ttc: maximum time-to-collision
+   */
   void setup()
   {
-    steer_limit = 0.1*M_PI; // max steering angle ~18 degrees. :(. I wanted to tokyo drift with the MuSHR car. 
+    steer_limit = 0.1*M_PI; // max steering angle ~18 degrees. :(. I wanted to drift with the MuSHR car. 
     wheelbase = agents[own_index].prob->params.wheelbase;
     agents[own_index].prob->params.steer_limit = steer_limit;
     agents[own_index].prob->params.vel_limit = speed_lim;
@@ -230,27 +322,22 @@ public:
     ROS_INFO("carrot_goal_dist: %f",cutoff_dist);
   }
 
-  void rpy_from_quat(float rpy[3],const geometry_msgs::PoseStamped::ConstPtr& msg) //not the best way to do it but I was getting errors when I tried to pass the pose.orientation cuz I don't understand the data type
-  { // you'd think that with ROS being such a widely used back-end there would be a function to convert quaternion to euler but nooooooooo (they do provide euler to quaternion tho) TODO: remove this comment for being "unacademic"
-    float q[4];
-    q[0] = msg->pose.orientation.x;
-    q[1] = msg->pose.orientation.y;
-    q[2] = msg->pose.orientation.z;
-    q[3] = msg->pose.orientation.w;
-    rpy[2] = atan2f(2.0f * (q[0] * q[1] + q[2] * q[3]),1 - 2 * (q[1] * q[1] + q[2] * q[2]));
-    rpy[0] = asinf(2.0f * (q[0] * q[2] - q[3] * q[1]));
-    rpy[1] = atan2f(2.0f * (q[0] * q[3] + q[1] * q[2]), 1.0f - 2.0f * (q[2] * q[2] + q[3] * q[3]));
-  }
-
+  /**
+   * local planner
+   *
+   * Calls the nhttc solver and sends the output of the solver to the ego-agent. Note that the solver solves only for one agent but it does need
+   * to know what the other agents are doing. 
+   */
   void plan()
   {
     // create obstacle list.
     obstacles = BuildObstacleList(agents);
-    agents[own_index].SetPlanTime(solver_time); //20 ms planning window
-    agents[own_index].SetObstacles(obstacles, size_t(own_index));
+    agents[own_index].SetPlanTime(solver_time); //20 ms planning window TODO: see if this only needs to be done once
+    agents[own_index].SetObstacles(obstacles, size_t(own_index)); // set the obstacles 
 
     Eigen::VectorXf controls = Eigen::VectorXf::Zero(2); //controls are 0,0 by default.
-    Eigen::VectorXf agent_state = agents[own_index].prob->params.x_0;
+    Eigen::VectorXf agent_state = agents[own_index].prob->params.x_0; //get agent's current state
+    //if we have a goal
     if(goal_received)
     { 
       // Find distance to current nhttc waypoint  
@@ -274,20 +361,17 @@ public:
         time_point_dist = multiplier*wp_vec.norm();
       }
 
-      float current_time = float((ros::Time::now() - begin).toSec());
-      cur_time_stamp = time_stamps[time_index];  
-      float delta_time = cur_time_stamp - current_time;
-      float virtual_dist = delta_time*speed_lim;
-      float dist_error = time_point_dist - virtual_dist;
-      float delta_speed = std::min(std::max(dist_error*10,-speed_lim),speed_lim*2);
-      agents[own_index].prob->params.vel_limit = speed_lim + delta_speed;
-      agents[own_index].prob->params.u_lb = Eigen::Vector2f(-(speed_lim + delta_speed), -steer_limit);
+      float current_time = float((ros::Time::now() - begin).toSec()); //the current time relative to the time at which the plans were published.
+      cur_time_stamp = time_stamps[time_index];  // time stamp by which the car must arrive at the time-waypoint. The time waypoint is simply the next closest waypoint in the plan.
+      float delta_time = cur_time_stamp - current_time; //time left to reach the time-waypoint
+      float virtual_dist = delta_time*speed_lim; // distance from the time-waypoint at which the car should be if it had to travel at the default speed
+      float dist_error = time_point_dist - virtual_dist; // error between the expected distance-to-go and the actual distance-to-go
+      float delta_speed = std::min(std::max(dist_error*10,-speed_lim),speed_lim*2); // a Proportional controller for changing the speed with some min-max limits
+      agents[own_index].prob->params.vel_limit = speed_lim + delta_speed; //set the new speed limit 
+      agents[own_index].prob->params.u_lb = Eigen::Vector2f(-(speed_lim + delta_speed), -steer_limit); // set the upper and lower bound corresponding to this limit
       agents[own_index].prob->params.u_ub = Eigen::Vector2f(speed_lim + delta_speed,steer_limit);
-      // if(fabs(dist_error)>=wheelbase*0.5)
-      // {
-      //   ROS_INFO("lag(+ve)/lead(-ve) in centimeters:%f",dist_error*100);
-      // }
-      if(dist > cutoff_dist) // 20 cm tolerance to goal
+
+      if(dist > cutoff_dist) // cutoff distance is the tolerance to goal. When goal-distance is less than cutoff, the goal is assumed to be reached
       {
         controls = agents[own_index].UpdateControls();
       }
@@ -296,9 +380,9 @@ public:
         if(current_wp_index < max_index-1)
         {
           agents[own_index].goal = waypoints[++current_wp_index];
-          viz_publish();
+          viz_publish(); // publish new goal point 
         }
-        if(dist < 0.3 and current_wp_index >= max_index-1)
+        if(dist < wheelbase and current_wp_index >= max_index-1) //condition for having reached the final waypoint. the cutoff distance for the final waypoint is less than the standard cutoff. This can later be extended to "must-pass" waypoints.
         {
           controls[0] = 0;
           controls[1] = 0;
@@ -306,7 +390,7 @@ public:
         }
         else
         {
-          controls = agents[own_index].UpdateControls(); // in case it is the final waypoint
+          controls = agents[own_index].UpdateControls(); // in case it is the final waypoint, keep going until dist-to-go is less than wheelbase
         }
       }
     }
@@ -315,9 +399,13 @@ public:
     send_commands(speed,steering_angle); //just sending out anything for now;
     return;
   }
-
 };
 
+/**
+ * main function
+ *
+ * Sets up the nhttc object and initializes everything.
+ */
 int main(int argc, char** argv)
 {
   ros::init(argc,argv,"nhttc_local_planner");
